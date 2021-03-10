4c22ee445c9c96f152c9cf19e95e9051e3fddf33
sitewellad <sitewellad@gmail.com>
2021-03-10 12:25:26 +0300

    fix: delete excess code

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,14 +2,12 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   let team = "";
-
   if (Array.isArray(members)) {
     members.forEach((word) => {
       if (typeof word == "string") {
         team = team + word.replace(/\s+/g, "").charAt(0);
       }
     });
-    upTeam = team.toUpperCase;
     return team.toUpperCase().split("").sort().join("");
   } else return false;
 };
